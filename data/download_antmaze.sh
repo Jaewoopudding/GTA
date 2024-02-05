e536c53d006cd2436107901de7e0045c46569b6b
sujin <yunsj0625@gmail.com>
2024-02-05 16:57:07 +0900

    remove antmaze

--- data/download_antmaze.sh
+++
@@ -1,8 +0,0 @@
-envs='antmaze-umaze-v2 antmaze-umaze-diverse-v2 antmaze-medium-diverse-v2 antmaze-medium-play-v2 antmaze-large-diverse-v2 antmaze-large-play-v2'
-
-for env in $envs
-do
-    echo $env
-    python data/download_d4rl_datasets.py --env_name=$env&
-    wait
-done
